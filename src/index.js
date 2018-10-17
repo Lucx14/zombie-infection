1e349b118d9b1ba09fcc365dbc211ebd0028ae86
Lucien Najev <luciennajev@hotmail.com>
2018-10-17 19:02:09 +0100

    chore: after some investigation online, all resources suggest in react apps that index.js file does not need to be tested so ive added a jest ignore to the package json

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// After some investigation online all resources are suggesting that index.js in a react app does not need to be unit tested and so i have added a jest ignore for it in the package.json
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
