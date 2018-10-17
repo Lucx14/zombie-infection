01d6920a8954c6a407bf8bd3592831f29038a597
James Sutherland <jrsutherland78@googlemail.com>
2018-10-17 12:05:42 +0100

    chore: changed functions and variables to private where needed

--- src/model/LocalGameModel.js
+++ src/model/LocalGameModel.js
@@ -1,47 +1,47 @@
 import { Player } from "./Player.js"
 
-export default function LocalGameModel() {
-  this.player = new Player()
-  this.canvas = document.getElementById("canvas");
-  this.canvas.width = 800;
-  this.canvas.height = 600;
-  this.canvasDraw = this.canvas.getContext("2d");
-  this.WIDTH = this.canvas.width;
-  this.HEIGHT = this.canvas.height;
-  this.keys = []
+export default function LocalGameModel(player = new Player()) {
+  this._player = player
+  this._canvas = document.getElementById("canvas");
+  this._canvas.width = 800;
+  this._canvas.height = 600;
+  this._canvasDraw = this._canvas.getContext("2d");
+  this._WIDTH = this._canvas.width;
+  this._HEIGHT = this._canvas.height;
+  this._keys = []
   this.gameSpeed = 15
 }
 
 LocalGameModel.prototype.tickDraw = function () {
-  setInterval(this.mainDraw.bind(this), this.gameSpeed);
+  setInterval(this._mainDraw.bind(this), this.gameSpeed);
 }
 
-LocalGameModel.prototype.mainDraw = function () {
-  this.canvasDraw.clearRect(0, 0, this.WIDTH, this.HEIGHT);
-  this.canvasDraw.fillStyle="red";
-  this.canvasDraw.beginPath();
-  this.canvasDraw.rect(this.player.x, this.player.y ,this.player.w , this.player.h);
-  this.canvasDraw.fill();
-  this.playerMovement()
+LocalGameModel.prototype._mainDraw = function () {
+  this._canvasDraw.clearRect(0, 0, this._WIDTH, this._HEIGHT);
+  this._canvasDraw.fillStyle="red";
+  this._canvasDraw.beginPath();
+  this._canvasDraw.rect(this._player.x, this._player.y ,this._player.w , this._player.h);
+  this._canvasDraw.fill();
+  this._playerMovement()
 }
 
-LocalGameModel.prototype.playerMovement = function(e) {
-  if (this.keys[87]) {
-    this.player.y --
+LocalGameModel.prototype._playerMovement = function(e) {
+  if (this._keys[87]) {
+    this._player.y --
   }
-  if (this.keys[83]) {
-    this.player.y ++
+  if (this._keys[83]) {
+    this._player.y ++
   }
-  if (this.keys[65]) {
-    this.player.x --
+  if (this._keys[65]) {
+    this._player.x --
   }
-  if (this.keys[68]) {
-    this.player.x ++
+  if (this._keys[68]) {
+    this._player.x ++
   }
   return false;
 };
 
 LocalGameModel.prototype.eventListen = function() {
-  this.canvas.addEventListener('keydown', function(e) { this.keys[e.keyCode] = true; }.bind(this));
-  this.canvas.addEventListener('keyup', function(e) { this.keys[e.keyCode] = false; }.bind(this));
+  this._canvas.addEventListener('keydown', function(e) { this._keys[e.keyCode] = true; }.bind(this));
+  this._canvas.addEventListener('keyup', function(e) { this._keys[e.keyCode] = false; }.bind(this));
 }
