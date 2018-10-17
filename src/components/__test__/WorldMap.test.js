fbe6a9f33d08550649dd6fc7d688f6e77229ee06
josh nickson <jnickson1506@gmail.com>
2018-10-17 11:17:11 +0100

    test: mock citylink component in world map unit test

--- src/components/__test__/WorldMap.test.js
+++ src/components/__test__/WorldMap.test.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import WorldMap from '../WorldMap.js'
 
+jest.mock('../CityLink', () => () => 
+  <div className="city-link">  
+    <button className="city-button"/>
+  </div>
+)
+
 describe('WorldMap', () => {
   let wrapper;
 
@@ -13,10 +19,15 @@ describe('WorldMap', () => {
     expect(wrapper.find('h1').text()).toMatch('World Map')
   });
 
-  // refactor this test to mock CityLink
   it('renders two buttons', () => {
     wrapper = mount(<WorldMap/>)
     expect(wrapper.find('.city-link button').length).toEqual(2)
+  });
+
+  it('populates grid on initial mount', () => {
+  });
+
+  it('infects a single cell on initial mount', () => {
   });
 
 });
