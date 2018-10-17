f778a3b88ab2bde70f7c9ee0e958baf64b3e42a3
Lucien Najev <luciennajev@hotmail.com>
2018-10-17 20:29:27 +0100

    chore: investigating ways to test the local game component

--- src/components/__test__/LocalGame.test.js
+++ src/components/__test__/LocalGame.test.js
@@ -3,14 +3,18 @@ import { shallow, mount } from 'enzyme';
 import LocalGame from '../LocalGame.js';
 
 describe('LocalGame', () => {
-  let wrapper;
 
-  beforeAll(() => {
-    wrapper = shallow(<LocalGame />);
+
+  it('renders a header', () => {
+    const wrapper = shallow(<LocalGame />);
+    expect(wrapper.find('h1').text()).toMatch('Local Map')
   });
-
-  // it('renders a header', () => {
-  //   wrapper = mount(<LocalMap />)
-  //   expect(wrapper.find('h1').text()).toMatch('Local Map')
-  // });
 });
+
+
+
+// what should we actually test here?
+// what we do is render a canvas to the page(via local game model)
+// but for some reason canvas appears as null to jest
+
+
