934d4e459ab3a4467ff9211f453362cb3ac11586
James Sutherland <jrsutherland78@googlemail.com>
2018-10-18 09:03:56 +0100

    feat: npc follows players when 'touched'

--- src/model/LocalGameModel.js
+++ src/model/LocalGameModel.js
@@ -1,7 +1,9 @@
 import { Player } from "./Player.js"
+import { Npc } from "./Npc.js"
 
 export default function LocalGameModel() {
   this.player = new Player()
+  this.npc = new Npc()
   this.canvas = document.getElementById("canvas");
   this.canvas.width = 800;
   this.canvas.height = 600;
@@ -18,13 +20,38 @@ LocalGameModel.prototype.tickDraw = function () {
 
 LocalGameModel.prototype.mainDraw = function () {
   this.canvasDraw.clearRect(0, 0, this.WIDTH, this.HEIGHT);
+
   this.canvasDraw.fillStyle="red";
   this.canvasDraw.beginPath();
-
   this.canvasDraw.rect(this.player.x, this.player.y ,this.player.w , this.player.h);
   this.canvasDraw.fill();
 
-  this.playerMovement()
+  if (this.npc.zombie === true) {
+    this.canvasDraw.fillStyle="green";
+  } else {
+    this.canvasDraw.fillStyle="black";
+  }
+
+  this.canvasDraw.beginPath();
+  this.canvasDraw.rect(this.npc.x, this.npc.y ,this.npc.w , this.npc.h);
+  this.canvasDraw.fill();
+
+  this.npcMovement();
+  this.playerMovement();
+}
+
+LocalGameModel.prototype.npcMovement = function() {
+  if ((this.npc.x >= this.player.x - 5 && this.npc.x <= this.player.x + 15) &&
+      (this.npc.y >= this.player.y - 5 && this.npc.y <= this.player.y + 15)) {
+    this.npc.zombie = true
+  }
+
+  if (this.npc.zombie === true) {
+    if (this.npc.x < this.player.x) {this.npc.x ++}
+    if (this.npc.x > this.player.x) {this.npc.x --}
+    if (this.npc.y < this.player.y) {this.npc.y ++}
+    if (this.npc.y > this.player.y) {this.npc.y --}
+  }
 }
 
 LocalGameModel.prototype.playerMovement = function(e) {
---
+++ src/model/Npc.js
@@ -0,0 +1,7 @@
+export function Npc() {
+  this.x = Math.random() * 800;
+  this.y = Math.random() * 600;
+  this.w = 10;
+  this.h = 10;
+  this.zombie = false
+}
