5a41e69f611b325f39d8f4632acbe75b6ab3516f
Lucien Najev <luciennajev@hotmail.com>
2018-10-19 14:52:58 +0100

    chore: updating app.js to add back all the city routes

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import {
   Route,
   HashRouter
 } from "react-router-dom";
+import LocalGame from './LocalGame';
 
 class App extends Component {
   render() {
@@ -12,6 +13,32 @@ class App extends Component {
         <div>
           <div>
             <Route exact path="/"  component={WorldMap}/>
+            <Route path="/london" render={() =><LocalGame/>}/>
+            <Route path="/oxford" render={() =><LocalGame/>}/>
+            <Route path="/paris" render={() =><LocalGame/>}/>
+            <Route path="/rome" render={() =><LocalGame/>}/>
+            <Route path="/oslo" render={() =><LocalGame/>}/>
+            <Route path="/reykjavik" render={() =><LocalGame/>}/>
+            <Route path="/new york" render={() =><LocalGame/>}/>
+            <Route path="/madrid" render={() =><LocalGame/>}/>
+            <Route path="/marrakech" render={() =><LocalGame/>}/>
+            <Route path="/cairo" render={() =><LocalGame/>}/>
+            <Route path="/nairobi" render={() =><LocalGame/>}/>
+            <Route path="/istanbul" render={() =><LocalGame/>}/>
+            <Route path="/dubai" render={() =><LocalGame/>}/>
+            <Route path="/cape town" render={() =><LocalGame/>}/>
+            <Route path="/los angeles" render={() =><LocalGame/>}/>
+            <Route path="/mexico city" render={() =><LocalGame/>}/>
+            <Route path="/bogota" render={() =><LocalGame/>}/>
+            <Route path="/rio de janeiro" render={() =><LocalGame/>}/>
+            <Route path="/tehran" render={() =><LocalGame/>}/>
+            <Route path="/new dehli" render={() =><LocalGame/>}/>
+            <Route path="/bangkok" render={() =><LocalGame/>}/>
+            <Route path="/shanghai" render={() =><LocalGame/>}/>
+            <Route path="/tokyo" render={() =><LocalGame/>}/>
+            <Route path="/hong kong" render={() =><LocalGame/>}/>
+            <Route path="/melbourne" render={() =><LocalGame/>}/>
+            <Route path="/wellington" render={() =><LocalGame/>}/>
           </div>
         </div>
       </HashRouter>
