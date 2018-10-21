ad9445fb6202804a48a4a45b7b1236960b81ccb0
James Sutherland <jrsutherland78@googlemail.com>
2018-10-21 19:34:26 +0100

    chore: fix blood splats, moved background to another canvas layer

--- src/components/LocalGame.js
+++ src/components/LocalGame.js
@@ -22,8 +22,10 @@ class LocalGame extends Component {
         <img id="person3" src="person-3.png" alt="" style={{display: "none"}}/>
         <img id="person4" src="person-4.png" alt="" style={{display: "none"}}/>
         <img id="person5" src="person-5.png" alt="" style={{display: "none"}}/>
+        <img id="bloodsplat" src="bloodSplat.png" alt="" style={{display: "none"}}/>
         <h1><center>Local Map</center></h1>
         <div id = "holder">
+          <canvas id="underCanvas" width="800" height="600"></canvas>
           <canvas id="canvas" width="800" height="600" tabIndex='1'></canvas>
         </div>
       </div>
--- src/model/LocalGameModel.js
+++ src/model/LocalGameModel.js
@@ -4,18 +4,28 @@ import { Npc } from "./Npc.js"
 export default function LocalGameModel(player = new Player(), npc = new Npc()) {
   this._player = player
   this._npc = npc
+
   this._canvas = document.getElementById("canvas");
   this._canvas.width = 800;
   this._canvas.height = 600;
   this._canvasDraw = this._canvas.getContext("2d");
   this._WIDTH = this._canvas.width;
   this._HEIGHT = this._canvas.height;
+
+  this._underCanvas = document.getElementById("underCanvas");
+  this._underCanvas.width = 800;
+  this._underCanvas.height = 600;
+  this._underCanvasDraw = this._underCanvas.getContext("2d");
+
   this._keys = []
   this._groupNpc = Array.from({length:300}, () => new Npc(Math.random() * 2))
   this.gameSpeed = 15
 
+  this._bloodsplats = []
+
   this._bg = document.getElementById("background")
   this._zombie = document.getElementById("zombie")
+  this._bloodsplat = document.getElementById("bloodsplat")
 
 }
 
@@ -28,6 +38,11 @@ LocalGameModel.prototype.tickDraw = function () {
 LocalGameModel.prototype._mainDraw = function () {
   const local = this
 
+  this._underCanvasDraw.drawImage(this._bg, 0, 0)
+  this._bloodsplats.forEach(function(bloodsplat) {
+    local._underCanvasDraw.drawImage(local._bloodsplat, bloodsplat.x - 5, bloodsplat.y + 20, 30, 20)
+  })
+
   this._canvasDraw.clearRect(0, 0, this._WIDTH, this._HEIGHT)
 
   this._groupNpc = this._groupNpc.sort(this._sortNpcs);
@@ -36,7 +51,9 @@ LocalGameModel.prototype._mainDraw = function () {
   this._canvasDraw.translate(-this._player.x, -this._player.y);
   this._canvasDraw.scale(2,2);
 
-  this._canvasDraw.drawImage(this._bg, 0,0)
+  this._underCanvasDraw.setTransform();
+  this._underCanvasDraw.translate(-this._player.x, -this._player.y);
+  this._underCanvasDraw.scale(2,2);
 
   this._canvasDraw.drawImage(this._zombie, this._player.x - 2.5, this._player.y - 12.5, 15, 25)
 
@@ -58,14 +75,16 @@ LocalGameModel.prototype._npcMovement = function() {
   const local = this
 
   this._groupNpc.forEach(function(npc) {
-    if (npc.isNear(local._player, 10)) {
+    if (npc.isNear(local._player, 10) && !npc.isInfected()) {
       npc.infect()
+      local._bloodsplats.push({x: npc.x - 2.5, y: npc.y - 12.5})
     }
 
     if (!npc.isInfected()) {
       local._groupNpc.forEach(function(otherNpcs) {
         if (otherNpcs.isInfected() && npc.isNear(otherNpcs, 10)) {
           npc.infect()
+          local._bloodsplats.push({x: npc.x - 2.5, y: npc.y - 12.5})
         }
       })
     }
@@ -86,7 +105,7 @@ LocalGameModel.prototype._npcMovement = function() {
           npc.move(otherNpcs, 'away')
         }
         // else if (!otherNpcs.isInfected() &&
-        //             npc.isNear(otherNpcs, 75)) {
+        //             npc.isNear(otherNpcs, 30)) {
         //   npc.move(otherNpcs, 'towards')
         // }
       }
