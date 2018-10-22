078cf6d20472c756e0888703ca1c1594c6fdc2b4
Lucien Najev <luciennajev@hotmail.com>
2018-10-22 20:36:54 +0100

    feature: adding css to town buttons

--- src/components/App.js
+++ src/components/App.js
@@ -63,7 +63,12 @@ class App extends Component {
   }
 
   renderButtons() {
-    const cities = this.state.playableCities
+    const cities = ["london","paris","rome","oslo",
+    "reykjavik","new york","madrid","marrakech",
+    "cairo","nairobi","istanbul" ,"dubai","cape town",
+    "los angeles","mexico city","bogota","rio de janeiro",
+    "tehran","new dehli","bangkok","shanghai","tokyo",
+    "hong kong","melbourne","wellington"]
     return(cities.map((city, index) => {
       return(
         <button className="city-button" 
