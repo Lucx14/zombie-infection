ebca54275d12f7f00c220df06b8db9e37500340f
Lucien Najev <luciennajev@hotmail.com>
2018-10-23 11:14:09 +0100

    feature: all cities overlayed onto the map

--- src/components/App.js
+++ src/components/App.js
@@ -66,9 +66,9 @@ class App extends Component {
     const cities = ["london","paris","rome","oslo",
     "reykjavik","new-york","madrid","marrakech",
     "cairo","nairobi","istanbul" ,"dubai","cape-town",
-    "los-angeles","mexico-city","bogota","rio de janeiro",
-    "tehran","new dehli","bangkok","shanghai","tokyo",
-    "hong kong","melbourne","wellington"]
+    "los-angeles","mexico-city","bogota","rio-de-janeiro",
+    "tehran","new-dehli","bangkok","shanghai","tokyo",
+    "hong-kong","melbourne","wellington", "vancouver"]
     return(cities.map((city, index) => {
       return(
         <button className="city-button" 
