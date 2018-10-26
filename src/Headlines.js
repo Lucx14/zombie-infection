69e7b3ce69fcd534d105335edf7e99062a0a4926
Lucien Najev <luciennajev@hotmail.com>
2018-10-26 10:53:02 +0100

    chore: headlines added

--- src/Headlines.js
+++ src/Headlines.js
@@ -2,7 +2,7 @@ const headlines = {
     "london": ["East London Pub attacked by Horde of Ravenous flesh-munchers", "Queen seen roaming Westminster in a tank wearing full-body armour", "Zombies infiltrate Buckingham Palace", "Reports of fallout from the Chernobyl disaster spreading and causing strange behaviour in young people", "Lewisham bus stop damaged", "afternoon screening of Aliens interrupted after zombie kids invade theatre", "Schools sending kids home early due to illness", "Doctors begin investigating contageous infection in children",  "UK reports small meteorite landing just outside of london, emergency services on the scene"],
     "paris": ["Zombies spotted sampling wine in local vineyard", "Holiday makers leave 2-star tripadvisor review due to zombie in soup", "Scientists in Paris working hard to identify cause of outbreak", "First case of Mad Cow disease identified", "Swiss punks on holiday in paris run for their lives as zombie hoard attack", "local patisserie starts selling ginger bread zombies", "Authorities to reconsider plans for a channel tunnel after concerns over UK disease outbreak"],
     "rome" : ["Romans swap spaghetti for human intestines", "Trevi fountain fills with corpses", "Walkers spotted in Tuscany", "Mamma Mia! The undead storm Milan fashion show", "Vatican City release declaration that the end of the world has begun", "Italian authorities heard hundreds of zombies into the colosseum"],
-    "oslo": ["Oslo resident begin city evacuation as zombie plague spreads"], 
+    "oslo": ["Oslo resident begin city evacuation as zombie plague spreads", "CDC report zombie infection spread rate is slower in colder climates"], 
     "reykjavik": ["Singer Bjork devoured by Zombie", "Undead unable to cope with Icelandic cold"],
     "new-york": ["Trick or treaters on the rampage in new york!", "Zombies hit Broadway", "Brooklyn resisdents fight back against the undead", "Madonna devoured by Zombie", "Michael Jackson bitten while filming 'Thriller' music video", "Ronald Reagan flees White House"],
     "madrid": ["Spanish authorities wake from siesta to discover the world is ending", "Populist politician in Madrid gains support as government fails to control outbreak", "Bullfight fright as bull re-animates from the dead", "King of Spain mauled by ravenous horde"], 
@@ -12,9 +12,9 @@ const headlines = {
     "istanbul" : ["Public services begin to break down in Istanbul as outbreak continues to spread"], 
     "dubai": ["Airports in Dubai shut down in attempt to contain outbreak", "Zombies melt in Qatari heat", "Zombie spotted eating human shawarma in Dubai", "Omani Sultan seeks refuge in mountain retreat"],
     "cape-town": ["resident report seeing what looked like a zombie shark swimming off the coast, scientists investigating how infection could spread from man to fish"], 
-    "los-angeles": ["L.A. nightlife disrupted by outbreak"], 
+    "los-angeles": ["L.A. nightlife disrupted by outbreak", "Disney World left a burning pile of rubble after military intervention to control outbreak"], 
     "mexico-city": ["Mexican drug cartel leader shits pants and has a heart attack after being cornered by crazed children in liquor store"], 
-    "bogota": ["World Health Organisation declare Bogota a no go zone"], 
+    "bogota": ["World Health Organisation declare Bogota a no go zone", "South American citizens evacuating densely populated areas"], 
     "rio-de-janeiro": ["Small band or Rio residents have started to fight back", "Zombies wage war in Rio de Janeiro favelas", "Hundreds of corpses seen floating down the Amazon", "Copacabana beach a feast of flesh for hungry Zombies", "Sao Paulo falls under Zombie rule"],
     "tehran": ["Iranian supreme leader declares he is super pissed off at inconvenient outbreak of zombie plague and is blaming America for everything", "After allowing trick or treating for the very first time, Iranian leaders vow 'Never Again!!"], 
     "new-dehli": ["Despite overwhelming odds, residents of New Delhi have deployed the military to protect citizens"], 
@@ -23,7 +23,7 @@ const headlines = {
     "tokyo": ["Filming of new Resident Evil movie suspended after real zombies invade set and kill the fake zombies"], 
     "hong-kong": ["A Hong Kong doctor has started working on a possible cure but it might be too late!"], 
     "melbourne": ["Melbourne house prices take a turn for the worse after town over-run with flesh eating children"], 
-    "wellington": ["Sleepy Wellington village residents lock themselves in a barn and radio for help"], 
+    "wellington": ["Sleepy Wellington village residents lock themselves in a barn and radio for help", "Residents reporting strange taste to their fush & chups"], 
     "vancouver": ["Reports of crazed kids on the rampage on halloween evening"]
   }
 
