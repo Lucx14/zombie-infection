697f3c659d78b0f061d7b2556d4ed67203c11d8e
James Sutherland <jrsutherland78@googlemail.com>
2018-10-27 14:37:04 +0100

    refactor: tests to accomodate changes to special ability checks

--- src/components/App.js
+++ src/components/App.js
@@ -88,11 +88,12 @@ class App extends Component {
   }
 
   specialAbility(ability) {
-    if (this.state.tokens > 29 && ability === "fishFrenzy" && !this.state.fishFrenzy && this.state.playableCities > 11) {
+    const level = this.state.playableCities.length
+    if (this.state.tokens > 29 && ability === "fishFrenzy" && !this.state.fishFrenzy && level > 11) {
       this.setState({ fishFrenzy: true, tokens: this.state.tokens -30 });
-    } else if (this.state.tokens > 39 && ability === "flyingZombies" && !this.state.flyingZombies && this.state.playableCities > 17) {
+    } else if (this.state.tokens > 39 && ability === "flyingZombies" && !this.state.flyingZombies && level > 17) {
       this.setState({ flyingZombies: true, tokens: this.state.tokens -40 });
-    } else if (this.state.tokens > 49 && ability === "worldWarZ" && !this.state.worldWarZ && this.state.playableCities > 23) {
+    } else if (this.state.tokens > 49 && ability === "worldWarZ" && !this.state.worldWarZ && level > 23) {
       this.setState({ worldWarZ: true, tokens: this.state.tokens -50 });
     }
   }
